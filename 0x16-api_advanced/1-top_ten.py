c642b83a495a889a68b001e3ea05705eecd2ace8
StephanieMfon <okpomfonstephanie@gmail.com>
2024-03-07 08:30:49 +0100

    Replaced file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Function to get a list of top 10 titles in a subreddit"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
-def top_ten(subreddit):
-  """Gets number of subs under a subreddit"""
-  url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-  headers = {
-    "User-Agent":"linux:0x16.api.advanced:v1.0.0 (by /u/No-Apartment7060)"
-  }
-  params = {
-    "limit":10
-  }
-  response = requests.get(url, headers=headers, params=params, Redirects=False)
 
-  if response.status_code == 404:
-    return 0
-  result = response.json().get("data")
-  [print(c.get('data').get('title') for c in result.get('children'))]
-  
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
