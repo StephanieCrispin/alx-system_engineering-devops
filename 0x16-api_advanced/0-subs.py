992d8317ad6023d0ca6ab95e74fba491147a24d5
StephanieMfon <okpomfonstephanie@gmail.com>
2024-03-19 22:58:34 +0100

    again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""
-This module queries the reddit API for the number of
-subscribers to a particular subreddit
-"""
+""" Exporting csv files"""
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    Sends a query to reddit api for the number of subscribers
-    """
-    api_url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    headers = {
-        "User-Agent": "CustomClient/1.0",
-    }
-
-    about = requests.get(api_url, headers=headers, allow_redirects=False)
-    if about.status_code != 200:
-        return 0
-    elif "subscribers" in about.json().get("data"):
-        return about.json().get("data").get("subscribers")
-    return 0
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
+    else:
+        return(0)
